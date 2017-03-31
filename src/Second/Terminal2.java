2aaa992917057fea26e1cad149ac4d4af47ec791
Dmytro_Kapeliukh <welcome.t.t100@gmail.com>
2017-03-31 14:43:07 +0300

    saadasd

--- src/Second/Terminal2.java
+++ src/Second/Terminal2.java
@@ -19,7 +19,7 @@ public class Terminal2 {
         System.out.println("How much product do you have?");
         int str = Integer.parseInt(reader.readLine());
         System.out.println("Please provide product parameters:\nProduct code, product price, special price of quantity, quantity for special price\nEach parameter in new line ");
-        for (int i = 0; i < str; i++){
+        for (int i = 0; i < str; i++) {
             String[] productDescription = new String[4];
             for (int k = 0; k < productDescription.length; k++) {
                 productDescription[k] = reader.readLine();
@@ -33,8 +33,7 @@ public class Terminal2 {
 
                 purchase.put(productDescription[0], products);
                 System.out.println("Done");
-            }
-            catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 System.out.println("Invalid parameter.");
             }
         }
@@ -50,31 +49,31 @@ public class Terminal2 {
         }
     }
 
-    public void scan(String item){
+    public void scan(String item) {
         total += this.updatePrice(purchase, item);
         System.out.printf("Item added. Current bill is: $%.2f. Please, scan the next item.\n(* to finish your purchase)\n", total);
     }
 
-    private float updatePrice(Map<String, Products> purchase, String item){
-        if (purchase.get(item).getQtyOption() - purchase.get(item).getCount() > 1){
-            purchase.get(item).updateCount(false);
-            return purchase.get(item).getPriceSingle();
-        }
-        else {
-            if (purchase.get(item).getQtyOption() - purchase.get(item).getCount() == 1){
-                float correction = purchase.get(item).getPriceOption() - purchase.get(item).getCount() * purchase.get(item).getPriceSingle();
-                purchase.get(item).updateCount(true);
-                return correction;
-            }
-            else {
+    private float updatePrice(Map<String, Products> purchase, String item) throws NullPointerException{
+
+            if (purchase.get(item).getQtyOption() - purchase.get(item).getCount() > 1) {
+                purchase.get(item).updateCount(false);
                 return purchase.get(item).getPriceSingle();
+            } else {
+                if (purchase.get(item).getQtyOption() - purchase.get(item).getCount() == 1) {
+                    float correction = purchase.get(item).getPriceOption() - purchase.get(item).getCount() * purchase.get(item).getPriceSingle();
+                    purchase.get(item).updateCount(true);
+                    return correction;
+                } else {
+                    return purchase.get(item).getPriceSingle();
+                }
             }
-        }
     }
 
     public void scanDiscount(String item){
         int value = (int) discount.get(item);
         updatePriceWithDiscount(value);
+        System.out.println("Another discount?\n* to finish your purchases");
     }
 
     private float updatePriceWithDiscount(int discount){
--- src/Second/TerminalTest.java
+++ src/Second/TerminalTest.java
@@ -3,6 +3,8 @@ package Second;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.math.BigDecimal;
+
 /**
  * Created by Dmytro_Kapeliukh on 3/30/2017.
  */
@@ -13,15 +15,87 @@ public class TerminalTest {
         Products testProduct = new Products("A", 1, Float.parseFloat("2.75"), 3);
         Products testProduct2 = new Products("B", Float.parseFloat("3.5"), Float.parseFloat("0"), 0);
         Products testProduct3 = new Products("C", Float.parseFloat("1.5"), Float.parseFloat("5"), 4);
+        Products testProduct4 = new Products("C", Float.parseFloat("0.75"), Float.parseFloat("0"), 0);
 
         terminal2.purchase.put("A", testProduct);
         terminal2.purchase.put("B", testProduct2);
         terminal2.purchase.put("C", testProduct3);
+        terminal2.purchase.put("D", testProduct4);
+
+        terminal2.discount.put("F", 5);
+        terminal2.discount.put("FF", 7);
+        terminal2.discount.put("FFF", 10);
 
         terminal2.scan("A");
         terminal2.scan("B");
         terminal2.scan("C");
+        terminal2.scan("D");
 
-        System.out.printf("Your purchase bill is: $%.2f.", terminal2.total);
+        terminal2.scanDiscount("F");
+
+        //double result = terminal2.total;
+        String list = String.format("%.2f", terminal2.total);
+
+        Assert.assertEquals(list, "6.41");
+    }
+
+    @Test
+    public void test1(){
+        Terminal2 terminal2 = new Terminal2();
+        Products testProduct = new Products("A", 1, Float.parseFloat("2.75"), 3);
+        Products testProduct2 = new Products("B", Float.parseFloat("3.5"), Float.parseFloat("0"), 0);
+        Products testProduct3 = new Products("C", Float.parseFloat("1.5"), Float.parseFloat("5"), 4);
+        Products testProduct4 = new Products("C", Float.parseFloat("0.75"), Float.parseFloat("0"), 0);
+
+        terminal2.purchase.put("A", testProduct);
+        terminal2.purchase.put("B", testProduct2);
+        terminal2.purchase.put("C", testProduct3);
+        terminal2.purchase.put("D", testProduct4);
+
+        terminal2.discount.put("F", 5);
+        terminal2.discount.put("FF", 7);
+        terminal2.discount.put("FFF", 10);
+
+        terminal2.scan("A");
+        terminal2.scan("A");
+        terminal2.scan("A");
+        terminal2.scan("D");
+
+        terminal2.scanDiscount("FF");
+
+        //double result = terminal2.total;
+        String list = String.format("%.2f", terminal2.total);
+
+        Assert.assertEquals(list, "3.26");
+    }
+
+    @Test
+    public void test2(){
+        Terminal2 terminal2 = new Terminal2();
+        Products testProduct = new Products("A", 1, Float.parseFloat("2.75"), 3);
+        Products testProduct2 = new Products("B", Float.parseFloat("3.5"), Float.parseFloat("0"), 0);
+        Products testProduct3 = new Products("C", Float.parseFloat("1.5"), Float.parseFloat("5"), 4);
+        Products testProduct4 = new Products("C", Float.parseFloat("0.75"), Float.parseFloat("0"), 0);
+
+        terminal2.purchase.put("A", testProduct);
+        terminal2.purchase.put("B", testProduct2);
+        terminal2.purchase.put("C", testProduct3);
+        terminal2.purchase.put("D", testProduct4);
+
+        terminal2.discount.put("F", 5);
+        terminal2.discount.put("FF", 7);
+        terminal2.discount.put("FFF", 10);
+
+        terminal2.scan("C");
+        terminal2.scan("C");
+        terminal2.scan("A");
+        terminal2.scan("A");
+
+        terminal2.scanDiscount("FFF");
+
+        //double result = terminal2.total;
+        String list = String.format("%.2f", terminal2.total);
+
+        Assert.assertEquals(list, "4.50");
     }
 }
